580ae13e628999f66751fe57be116fa84663f943
Leigh Salvador <lssalvador@csu.fullerton.edu>
2018-03-09 23:04:03 -0800

    first

--- main.js
+++ main.js
@@ -12,15 +12,18 @@ app.set('port', (process.env.PORT || 5000))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-// ROUTES
 
+
+//=== ROUTES ==============================================
 app.get('/', function(req, res) {
-	res.send("Hi I am a chatbot")
+	res.send(".")
 })
 
 let token = "EAAH59xTPWIsBAMhZBTZAbg7GmWrmWTgeU5xANUSEgOrtVp4bNhHCjLeQi0877Sf0AoVm79HvMeGZAhbWoLvFyPejefZA94AZAIVKzFvhwEN18PThiZCnPSeR5ozBRBJkjWdeIOCwLLabtGPnj9WPHnovxP1Oyc0xooTEADQk7G3gZDZD"
 
-// Facebook
+//=== FACEBOOK =============================================
+
+var
 
 app.get('/webhook/', function(req, res) {
 	if (req.query['hub.verify_token'] === "EAAH59xTPWIsBAMhZBTZAbg7GmWrmWTgeU5xANUSEgOrtVp4bNhHCjLeQi0877Sf0AoVm79HvMeGZAhbWoLvFyPejefZA94AZAIVKzFvhwEN18PThiZCnPSeR5ozBRBJkjWdeIOCwLLabtGPnj9WPHnovxP1Oyc0xooTEADQk7G3gZDZD") {
@@ -30,10 +33,51 @@ app.get('/webhook/', function(req, res) {
 })
 
 app.post('/webhook/', function(req, res) {
+
+  curl -X POST -H "Content-Type: application/json" -d '{
+    "recipient":{
+      "id":"<PSID>"
+    },
+    "message":{
+      "attachment":{
+        "type":"template",
+        "payload":{
+          "template_type":"generic",
+          "elements":[
+             {
+              "title":"Welcome!",
+              "image_url":"https://petersfancybrownhats.com/company_image.png",
+              "subtitle":"We have the right hat for everyone.",
+              "default_action": {
+                "type": "web_url",
+                "url": "https://petersfancybrownhats.com/view?item=103",
+                "messenger_extensions": false,
+                "webview_height_ratio": "tall",
+                "fallback_url": "https://petersfancybrownhats.com/"
+              },
+              "buttons":[
+                {
+                  "type":"web_url",
+                  "url":"https://petersfancybrownhats.com",
+                  "title":"View Website"
+                },{
+                  "type":"postback",
+                  "title":"Start Chatting",
+                  "payload":"DEVELOPER_DEFINED_PAYLOAD"
+                }
+              ]
+            }
+          ]
+        }
+      }
+    }
+  }' "https://graph.facebook.com/v2.6/me/messages?access_token="EAAH59xTPWIsBAMhZBTZAbg7GmWrmWTgeU5xANUSEgOrtVp4bNhHCjLeQi0877Sf0AoVm79HvMeGZAhbWoLvFyPejefZA94AZAIVKzFvhwEN18PThiZCnPSeR5ozBRBJkjWdeIOCwLLabtGPnj9WPHnovxP1Oyc0xooTEADQk7G3gZDZD>"
+
 	let messaging_events = req.body.entry[0].messaging
 	for (let i = 0; i < messaging_events.length; i++) {
 		let event = messaging_events[i]
 		let sender = event.sender.id
+
 		if (event.message && event.message.text) {
 			let text = event.message.text
 			sendText(sender, "Text echo: " + text.substring(0, 100))
@@ -61,6 +105,7 @@ function sendText(sender, text) {
 	})
 }
 
+// === PORT =======================================
 app.listen(app.get('port'), function() {
 	console.log("running: port")
 })
